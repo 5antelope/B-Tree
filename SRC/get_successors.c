ca87f3604ea479ec0c503e601c86015acb8697fe
wy4515 <y.wu4515@gmail.com>
2015-02-12 21:54:50 -0500

    first commit

---
+++ SRC/get_successors.c
@@ -0,0 +1,11 @@
+#include "def.h"
+
+
+int get_successors(char *key, int k, char *result[]) {
+    key = (void *)'0';
+    k = 0;
+    result = (void *)'0';
+    printf("IMPLEMENT  ME\n");
+
+    return 0;
+}
